bf43830ca3fa6523132a9b72e6e96f94f531ec6b
Ashish <ashishnagar1221@gmail.com>
2025-08-07 00:11:10 +0530

    Fix error from chatgpt

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,26 +1,7 @@
-"""from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
-@api_view(["GET"])
-def home(request):
-    return Response({"message": "Welcome to Blog API!"})
-"""
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import permission_classes
-
-class RegisterView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-
-        if User.objects.filter(username=username).exists():
-            return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        user = User.objects.create_user(username=username, password=password)
-        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all().order_by('-created_at')
+    serializer_class = BlogPostSerializer
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -1,4 +1,4 @@
-'''from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import BlogPostViewSet
@@ -10,22 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
-'''
-from django.contrib import admin
-from django.urls import path
-from blog.views import home, RegisterView, BlogListCreateView, BlogDetailView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [
-    path('', home),  # 👈 root URL par hello message
-    path('admin/', admin.site.urls),
-
-    # Auth
-    path('api/register/', RegisterView.as_view()),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Blog
-    path('api/posts/', BlogListCreateView.as_view()),
-    path('api/posts/<int:pk>/', BlogDetailView.as_view()),
-]
